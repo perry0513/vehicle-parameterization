907dd304dadd1d14be06066aeb08d9fe487c9fea
elam <elamdf@berkeley.edu>
2022-04-21 14:45:52 -0700

    added packing solver

--- uuv.py
+++ uuv.py
@@ -1,10 +1,11 @@
 import math
+from py3dbp import Packer, Bin, Item
 
 df_threshold = 90
 energy_needed = 260 # kWh
 
 
-# constants 
+# constants
 payload_x = 1000 # mm
 payload_y = 1000 # mm
 payload_z = 300 # mm
@@ -19,9 +20,12 @@ fairing_thickness = 0.0127 # m
 fairing_density = 1465 # kg/m^3
 float_density = 465 # kg/m^3
 
-def pack(_length, _vehicle_volume, _fairing_area):
-    length = _length * 1e-3 # m
-    vehicle_volume = _vehicle_volume * 1e-9 # m^3
+def pack(sol):
+    params = sol.params
+    length = params[0] * 1e-3 # m
+    vehicle_volume = params[0] * params[1] * params[2] * 1e-9 # m^3
+    _fairing_area = sol.area
+    fairing_volume = sol.vol
 
     # Assume batteries and pressure vessels are rectangles
 
@@ -30,9 +34,9 @@ def pack(_length, _vehicle_volume, _fairing_area):
     bat_mass = bat_volume * bat_density # kg
     bat_length = length # m
     bat_width = math.sqrt(bat_volume / length) # m
-    bat_height = math.sqrt(bat_volume / length) # m
+    bat_depth = math.sqrt(bat_volume / length) # m
 
-    PV_disp_volume = (bat_length + PV_thickness) * (bat_width + PV_thickness) * (bat_height + PV_thickness) # m^3
+    PV_disp_volume = (bat_length + PV_thickness) * (bat_width + PV_thickness) * (bat_depth + PV_thickness) # m^3
     PV_volume = PV_disp_volume - bat_volume # m^3
     PV_mass = PV_volume * PV_density # kg
     PV_buoyancy = PV_disp_volume * water_density # kg
@@ -51,6 +55,12 @@ def pack(_length, _vehicle_volume, _fairing_area):
     float_volume = total_in_water_weight / (water_density - float_density) # m^3
     spare_volume = vehicle_volume - fairing_disp_volume - PV_disp_volume - payload_volume # m^3
     is_neutrally_buoyant = spare_volume > float_volume
-    
 
     # TODO: check whether PV and payload can fit in vehicle
+    packer = Packer()
+    packer.add_bin(Bin('main-cabin', params[0], params[1], params[2], 10))
+    packer.add_item(Item("pv", bat_length + PV_thickness, bat_width + PV_thickness, bat_depth + PV_thickness, 0)) # not using weight for packing
+    packer.add_item(Item("payload", payload_x, payload_y, payload_z, 0)) # not using weight for packing
+    packer.pack()
+
+    return is_neutrally_buoyant and len(packer.bins[0].unfitted_items) == 0
