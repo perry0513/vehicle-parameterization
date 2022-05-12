8120dc9bd74b42443d75a77fc15f107746500252
perry0513 <perrypeiweichen@gmail.com>
2022-05-11 17:15:52 -0700

    Allow input json file

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ norms="none best_valid rolling_avg"
 for norm in $norms; do
     for sched in $scheds; do
         for i in {1..5}; do 
-            python sa.py --json json/uuv_0300.json --sched $sched --norm $norm --log --visual --suffix $i
+            python sa.py --json $1 --sched $sched --norm $norm --log --visual --suffix $i
         done
     done
 done
